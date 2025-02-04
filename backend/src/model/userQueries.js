243b4ced2a3bcea36a03d74b448626afbea1e278
mastachii <mastachii273@gmail.com>
2025-02-04 13:31:17 +0800

    feat: Query for creating new users

--- backend/src/model/userQueries.js
+++ backend/src/model/userQueries.js
@@ -3,13 +3,26 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 class User {
-    async createUser({ username, displayName, profilePicture }) {
-        await prisma.user.create({
+    async createUser({ username, email, password, displayName, profilePicture }) {
+        const user = await prisma.user.create({
             data: {
                 username,
+                email,
+                password,
                 displayName,
                 profilePicture,
             },
         });
+
+        return user;
     }
 }
+
+const user = new User();
+
+user.createUser({
+    username: "mastachii273",
+    email: "mastachii273@gmail.com",
+    password: "alsaliasid12",
+    displayName: "machii",
+});
