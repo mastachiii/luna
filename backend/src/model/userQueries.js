b032261d25908270a4b00ae3e668d0afa1de8b2c
mastachii <mastachii273@gmail.com>
2025-02-04 13:42:13 +0800

    wip: Update user model when sending friend requests

--- backend/src/model/userQueries.js
+++ backend/src/model/userQueries.js
@@ -16,13 +16,47 @@ class User {
 
         return user;
     }
+
+    // Update the user requests first then update the user who will receive that request...
+    async addUser({ id, username }) {
+        await prisma.user.update({
+            where: { id },
+            data: {
+                requestsSent: {
+                    connect: {
+                        username,
+                    },
+                },
+            },
+        });
+
+        await prisma.user.update({
+            where: { username },
+            data: {
+                requestsReceived: {
+                    connect: {
+                        id,
+                    },
+                },
+            },
+        });
+    }
+
+    async getUser({ id }) { }
 }
 
 const user = new User();
 
-user.createUser({
-    username: "mastachii273",
-    email: "mastachii273@gmail.com",
-    password: "alsaliasid12",
-    displayName: "machii",
-});
+// user.createUser({
+//     username: "mastachii273",
+//     email: "mastachii273@gmail.com",
+//     password: "alsaliasid12",
+//     displayName: "machii",
+// });
+
+// user.createUser({
+//     username: "Midori",
+//     email: "midori@gmail.com",
+//     password: "alsaliasid12",
+//     displayName: "midori :-)",
+// });
