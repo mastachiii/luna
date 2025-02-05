451c155cb0f88885ca0afd52f49c7d5fc5bb3cd6
mastachii <mastachii273@gmail.com>
2025-02-05 23:23:02 +0800

    wip: routes

---
+++ backend/src/app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => res.send("Hello Express"));
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
