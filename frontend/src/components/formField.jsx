01c8cc060d70982cdf0bb360d975861ae6da6fde
mastachii <mastachii273@gmail.com>
2025-02-12 11:33:10 +0800

    wip: Styling login page

--- frontend/src/components/formField.jsx
+++ frontend/src/components/formField.jsx
@@ -1,7 +1,9 @@
 export default function FormField({ type, name, label, value, valueHandler }) {
     return (
         <div className="flex flex-col pl-6 pr-6">
-            <label htmlFor={name} className="text-start">{label}</label>
+            <label htmlFor={name} className="mb-1 mt-4 text-[10px] text-start text-zinc-50 font-semibold tracking-wider ">
+                {label}
+            </label>
             <input
                 type={type}
                 name={name}
@@ -9,7 +11,7 @@ export default function FormField({ type, name, label, value, valueHandler }) {
                 onChange={e => {
                     valueHandler(e.target.value);
                 }}
-                className=" p-2 text-sm bg-zinc-800 border-1 border-zinc-900 outline-0"
+                className=" p-2 mb-3 text-sm text-zinc-50 bg-zinc-800 border-1 border-zinc-900 rounded-sm outline-0"
             />
         </div>
     );
--- frontend/src/components/logIn.jsx
+++ frontend/src/components/logIn.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import userApi from "../helpers/userApi";
 import FormField from "./formField";
+import { Link } from "react-router";
 
 export default function LogIn() {
     const [username, setUsername] = useState("");
@@ -15,12 +16,26 @@ export default function LogIn() {
 
     return (
         <div className="w-screen h-screen flex align-middle bg-amber-200">
-            <form onSubmit={handleSubmit} className=" w-xl flex flex-col justify-center text-center m-auto p-5 bg-zinc-700 font-noto">
-                <h4 className="text-2xl font-normal text-zinc-50 ">Welcome back!</h4>
-                <p className="mb-2 text-neutral-400">We're so exicited to see you again!</p>
-                <FormField type={"text"} name={"username"} label={"Username: "} value={username} valueHandler={setUsername} />
-                <FormField type={"password"} name={"password"} label={"Password: "} value={password} valueHandler={setPassword} />
-                <button type="submit">Log in</button>
+            <form
+                onSubmit={handleSubmit}
+                className=" w-xl flex flex-col justify-center text-center m-auto pt-10 pb-12 pl-3 pr-3 rounded-sm bg-zinc-700 font-noto"
+            >
+                <h4 className="text-xl font-semibold text-zinc-50 ">Welcome back!</h4>
+                <p className="mb-2 text-sm  text-neutral-400">We're so excited to see you again!</p>
+                <FormField type={"text"} name={"username"} label={"USERNAME "} value={username} valueHandler={setUsername} />
+                <FormField type={"password"} name={"password"} label={"PASSWORD "} value={password} valueHandler={setPassword} />
+                <button
+                    type="submit"
+                    className="bg-pink-300 ml-6 mr-6 mt-5 pt-3 pb-3 text-sm text-zinc-50 font-semibold rounded-sm cursor-pointer hover:bg-pink-700 transition duration-200 ease-in-out"
+                >
+                    Log in
+                </button>
+                <p className="ml-6 mt-3 text-xs text-start text-neutral-400 ">
+                    Need an account?{" "}
+                    <Link to="/sign-up" className="text-pink-400  hover:underline">
+                        Register
+                    </Link>
+                </p>
             </form>
         </div>
     );
