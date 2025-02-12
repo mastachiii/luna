e5e748e1e0f0c8d2a91a327fd4e684d5b91bcbe3
mastachii <mastachii273@gmail.com>
2025-02-13 00:06:21 +0800

    wip: more stylinggggg

--- frontend/src/components/navBar.jsx
+++ frontend/src/components/navBar.jsx
@@ -7,28 +7,37 @@ export default function NavBar({ componentHandler, groupIdHandler, groupData }) 
     const [selected, setSelected] = useState(true);
 
     return (
-        <div className=" h-screen flex flex-col pl-3 pr-3 bg-neutral-200">
+        <div className="h-screen flex flex-col items-center pl-3 pr-3 bg-neutral-200">
             <button
                 onMouseEnter={() => setUserBtnHover(true)}
                 onMouseLeave={() => setUserBtnHover(false)}
                 onClick={() => setSelected(null)}
-                className={`w-13 mt-2 p-2  rounded-full hover:rounded-xl ${!selected ? "rounded-xl bg-pink-300" : "bg-zinc-50"}`}
+                className={`w-13 h-13 mt-3 p-2  rounded-full cursor-pointer transition duration-200 ease-in group hover:rounded-xl hover:bg-pink-300 hover:translate-x-1 ${
+                    !selected ? "rounded-xl bg-pink-300" : "bg-zinc-50"
+                }`}
             >
-                <img src={userBtnHover || !selected ? logo : logoUnfocused} alt="logo" />
+                <img src={userBtnHover || !selected ? logo : logoUnfocused} alt="logo" className={` ${selected && "group-hover:animate-bounce"}`} />
             </button>
+            <div className="w-8 h-[0.5px] mt-2  bg-zinc-400"></div>
             {groupData.map(c => {
                 return (
-                    <button
-                        key={c.id}
-                        onClick={() => {
-                            componentHandler("group");
-                            groupIdHandler(c.id);
-                        }}
-                    >
-                        <img src={c.picture} alt="group profile" className="w-13" />
-                    </button>
+                    <div key={c.id}>
+                        <button
+                            onClick={() => {
+                                componentHandler("group");
+                                groupIdHandler(c.id);
+                            }}
+                            className="bg-amber-200 w-13 h-13 p-2 mt-2 rounded-full transition-all duration-100 ease-in  hover:rounded-xl hover:translate-x-1 "
+                        >
+                            <img src={c.picture} alt="group profile" className="w-13" />
+                        </button>
+                        <span className="absolute top-23 left-30">{c.name}</span>
+                    </div>
                 );
             })}
+            <button>
+                <p className="text-2xl font-semibold">+</p>
+            </button>
         </div>
     );
 }
