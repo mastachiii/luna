e9fe32281053838d29bdb5d66da625e7d9ba5348
mastachii <mastachii273@gmail.com>
2025-02-13 12:23:39 +0800

    wip: Styling chat/friend nav bar

--- frontend/src/components/chatNavBar.jsx
+++ frontend/src/components/chatNavBar.jsx
@@ -1,25 +1,34 @@
+import ChatNavBarButton from "./chatNavBarButton";
+import friendsLogo from "../assets/friends.svg";
+import shop from "../assets/shop.svg";
+import nitro from "../assets/nitro.svg";
+
 export default function ChatNavBar({ friends, compHandler, friendIdHandler }) {
     return (
-        <div>
-            <button
-                onClick={() => {
-                    compHandler("friend list");
-                }}
-            >
-                Friends
-            </button>
+        <div className="w-62 flex flex-col bg-neutral-100">
+            <div className="w-full h-[10px] pt-5 pb-5 mb-5 border-b-2 border-zinc-200 shadow-md shadow-zinc-200 "></div>
+            <div className="flex flex-col gap-1">
+                <ChatNavBarButton
+                    handleClick={() => {
+                        compHandler("friend list");
+                    }}
+                    image={friendsLogo}
+                    label={"Friends"}
+                />
+                <ChatNavBarButton image={shop} label={"Shop"} />
+                <ChatNavBarButton image={nitro} label={"Nitro"} />
+            </div>
             {friends.map(f => {
                 return (
-                    <div
-                        onClick={() => {
+                    <ChatNavBarButton
+                        handleClick={() => {
                             compHandler("chat friend");
                             friendIdHandler(f.id);
                         }}
+                        image={f.profilePicture}
+                        label={f.displayName}
                         key={f.id}
-                    >
-                        <img src={f.profilePicture} alt="friend profile picture" className="size-5" />
-                        <p>{f.displayName}</p>
-                    </div>
+                    />
                 );
             })}
         </div>
--- frontend/src/components/chatNavBarButton.jsx
+++ frontend/src/components/chatNavBarButton.jsx
@@ -1,7 +1,7 @@
 export default function ChatNavBarButton({ handleClick, image, label }) {
     return (
-        <button onClick={handleClick}>
-            <img src={image} alt="" />
+        <button onClick={handleClick} className="bg-amber-200 flex ml-3 mr-3 p-3 rounded-sm">
+        <img src={image} className="size-7" />
             <p>{label}</p>
         </button>
     );
