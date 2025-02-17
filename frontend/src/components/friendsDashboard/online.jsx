81cbe1a9a4ebe165bdbd2f5af42efc0272f371bb
mastachii <mastachii273@gmail.com>
2025-02-17 17:50:46 +0800

    feat: Component for showing friends and removing them

--- frontend/src/components/friendsDashboard/online.jsx
+++ frontend/src/components/friendsDashboard/online.jsx
@@ -1,5 +1,25 @@
+import { useState, useRef } from "react";
+import userApi from "../../helpers/userApi";
+
+function RemoveFriendDialog({ friend, show, ref }) {
+    function handleRemoveFriend() {
+        userApi.removeFriend({ id: friend.id });
+    }
+
+    return (
+        <dialog ref={ref}>
+            <button onClick={() => ref.current.close()}>X</button>
+            <p>Are you sure you want to remove blahblah</p>
+            <button onClick={handleRemoveFriend}>YES</button>
+        </dialog>
+    );
+}
+
 export default function Online({ friends, compHandler, friendHandler }) {
-    console.log(friends);
+    const [optionsActive, setOptionsActive] = useState(false);
+    const [dialogActive, setDialogActive] = useState(false);
+    const dialogRef = useRef();
+
     return (
         <div>
             {friends.map(f => {
@@ -14,6 +34,13 @@ export default function Online({ friends, compHandler, friendHandler }) {
                         >
                             CHAT
                         </button>
+                        <div>
+                            <button onClick={() => setOptionsActive(!optionsActive)}>OOO</button>
+                            <div className={`${optionsActive ? "block" : "hidden"}`}>
+                                <button onClick={() => dialogRef.current.showModal()}>Remove friend</button>
+                            </div>
+                        </div>
+                        <RemoveFriendDialog show={dialogActive} ref={dialogRef} friend={f} />
                     </div>
                 );
             })}
