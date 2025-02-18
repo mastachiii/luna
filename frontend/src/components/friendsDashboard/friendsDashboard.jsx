6260982ee82989d5a11c091e966398395cb1e419
mastachii <mastachii273@gmail.com>
2025-02-18 11:52:54 +0800

    feat: Styled friend dashboard nav

--- frontend/src/components/friendsDashboard/friendsDashboard.jsx
+++ frontend/src/components/friendsDashboard/friendsDashboard.jsx
@@ -5,9 +5,14 @@ import FriendRequests from "./friendRequests";
 import AddFriend from "./addFriend";
 import friends from "../../assets/friends.svg";
 
-function Button({ handler, label }) {
+function Button({ handler, label, condition }) {
     return (
-        <button onClick={handler} className="text-zinc-200">
+        <button
+            onClick={handler}
+            className={`pt-1 pb-1 pl-2 pr-2 rounded-md cursor-pointer ${
+                label !== "Add Friend" ? (condition ? "bg-zinc-200 text-black" : "text-zinc-700 hover:bg-zinc-200") : ""
+            } ${label === "Add Friend" ? (condition ? "bg-none text-green-700" : "bg-green-700 text-white") : ""}`}
+        >
             {label}
         </button>
     );
@@ -59,14 +64,14 @@ export default function FriendsDashboard({ compHandler, friendHandler, selHandle
             <div className="w-full h-12.5 flex items-center shadow-md shadow-zinc-200 text-sm">
                 <span className="flex items-center ml-5 gap-2">
                     <img src={friends} alt="" className="size-7" />
-                    <p className="font-semibold">Friends</p>
+                    <p className="font-semibold text-zinc-800">Friends</p>
                 </span>
                 <div className="w-[1px] h-[50%] ml-3 mr-3  bg-zinc-300 "></div>
                 <div className="flex gap-5 ml-1">
-                    <Button handler={() => setCompToRender("online")} label={"Online"} />
-                    <Button handler={() => setCompToRender("all")} label={"All"} />
-                    <Button handler={() => setCompToRender("pending")} label={"Pending"} />
-                    <button onClick={() => setCompToRender("add")}>Add Friend</button>
+                    <Button handler={() => setCompToRender("online")} label={"Online"} condition={compToRender === "online"} />
+                    <Button handler={() => setCompToRender("all")} label={"All"} condition={compToRender === "all"} />
+                    <Button handler={() => setCompToRender("pending")} label={"Pending"} condition={compToRender === "pending"} />
+                    <Button handler={() => setCompToRender("add")} label={"Add Friend"} condition={compToRender === "add"} />
                 </div>
             </div>
             {comp}
