5c9fb26642181e1d3ea872209e3696c00f9aaa73
mastachii <mastachii273@gmail.com>
2025-02-18 18:59:51 +0800

    feat: Styled remove friend dialog

--- frontend/src/components/friendsDashboard/friendList.jsx
+++ frontend/src/components/friendsDashboard/friendList.jsx
@@ -2,6 +2,29 @@ import { useState, useRef } from "react";
 import userApi from "../../helpers/userApi";
 import User from "./user";
 
+function RemoveFriendDialog({ friends, friendsHandler, id, ref }) {
+    const friend = friends.find(f => f.id === id) || {};
+
+    function handleRemoveFriend() {
+        userApi.removeFriend({ id: friend.id });
+        friendsHandler(friends.filter(f => f.id !== friend.id));
+        ref.current.close();
+    }
+
+    return (
+        <dialog ref={ref} className={`m-auto  rounded-md`}>
+            <h4 className="text-lg font-semibold mb-0 p-3 pl-4">Remove '{friend.displayName}'</h4>
+            <p className="text-sm p-3 pl-4">
+                Are you sure you want to remove <b className="font-semibold">{friend.displayName}</b> from your friends?
+            </p>
+            <div className="h-15 flex justify-end items-center gap-5 mt-5 pr-4 bg-zinc-100">
+                <button onClick={() => ref.current.close()} className="text-sm cursor-pointer hover:underline">Cancel</button>
+                <button onClick={handleRemoveFriend} className="p-2 pl-3 pr-3 text-sm text-white rounded-sm  bg-red-500 cursor-pointer hover:bg-red-700">Remove Friend</button>
+            </div>
+        </dialog>
+    );
+}
+
 export default function FriendList({ friends, compHandler, friendHandler, selHandler, label }) {
     const [activeId, setActiveId] = useState(false);
     const [friendsToShow, setFriendsToShow] = useState(friends); // If user removes a friend, update state. Forcing the page to reload would be messier..
@@ -20,51 +43,36 @@ export default function FriendList({ friends, compHandler, friendHandler, selHan
         setFriendsToShow(friends.filter(f => f.displayName.includes(e.target.value) || f.username.includes(e.target.value)));
     }
 
-    if (friends) {
-        const friend = friends.find(f => f.id === activeId) || {};
-
-        return (
-            <div className="w-[70%] p-10">
-                <input
-                    type="text"
-                    value={search}
-                    onChange={handleSearch}
-                    placeholder={"Search"}
-                    className="w-full p-2 pl-3 rounded-sm text-sm  bg-zinc-200 outline-0"
-                />
-                <div>
-                    <p className="mt-5 mb-3 ml-1 text-xs font-semibold text-zinc-600">
-                        {label} - {friendsToShow.length}
-                    </p>
-                    <div className="w-full h-[1px] ml-1 mb-3 bg-zinc-200"></div>
-                </div>
-                {friendsToShow.map(f => {
-                    return (
-                        <User
-                            user={f}
-                            friendHandler={friendHandler}
-                            compHandler={compHandler}
-                            selHandler={selHandler}
-                            optionsHandler={handleOptionsClick}
-                            condition={activeId === f.id}
-                            ref={dialogRef}
-                            key={f.id}
-                        />
-                    );
-                })}
-                <dialog ref={dialogRef} className="m-auto">
-                    <div className="m-auto">
-                        <h4 className="text-lg font-semibold mb-0 p-3 pl-4">Remove '{friend.displayName}'</h4>
-                        <p className="text-sm p-3 pl-4">
-                            Are you sure you want to remove <b className="font-semibold">{friend.displayName}</b> from your friends?
-                        </p>
-                        <div className="mt-auto h-[30%] p-0">
-                            <button onClick={() => dialogRef.current.close()}>Cancel</button>
-                            <button>Remove Friend</button>
-                        </div>
-                    </div>
-                </dialog>
+    return (
+        <div className="w-[70%] p-10">
+            <input
+                type="text"
+                value={search}
+                onChange={handleSearch}
+                placeholder={"Search"}
+                className="w-full p-2 pl-3 rounded-sm text-sm  bg-zinc-200 outline-0"
+            />
+            <div>
+                <p className="mt-5 mb-3 ml-1 text-xs font-semibold text-zinc-600">
+                    {label} - {friendsToShow.length}
+                </p>
+                <div className="w-full h-[1px] ml-1 mb-3 bg-zinc-200"></div>
             </div>
-        );
-    }
+            {friendsToShow.map(f => {
+                return (
+                    <User
+                        user={f}
+                        friendHandler={friendHandler}
+                        compHandler={compHandler}
+                        selHandler={selHandler}
+                        optionsHandler={handleOptionsClick}
+                        condition={activeId === f.id}
+                        ref={dialogRef}
+                        key={f.id}
+                    />
+                );
+            })}
+            {activeId && <RemoveFriendDialog friends={friendsToShow} friendsHandler={setFriendsToShow} ref={dialogRef} id={activeId} />}
+        </div>
+    );
 }
