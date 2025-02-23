b46676c5c5fa6f13e267739a644fcfa3ca476062
mastachii <mastachii273@gmail.com>
2025-02-23 18:28:42 +0800

    wip: Dark mode for group edit component

--- frontend/src/components/editorUserList.jsx
+++ frontend/src/components/editorUserList.jsx
@@ -6,7 +6,7 @@ export default function EditorUserList({ label, inputValue, inputHandler, childr
             <div className="w-[90%] h-[1px] mt-3 ml-1 mb-3 bg-zinc-100"></div>
             <div className={`pl-3 mb-10`}>
                 <span className="flex items-center gap-2">
-                    <p className="text-xs font-semibold text-zinc-700">{label}</p>
+                    <p className="text-xs font-semibold text-zinc-700 dark:text-zinc-200">{label}</p>
                     <button
                         onClick={() => {
                             showHandler(!show);
@@ -21,7 +21,7 @@ export default function EditorUserList({ label, inputValue, inputHandler, childr
                         value={inputValue}
                         onChange={(e) => inputHandler(e.target.value)}
                         placeholder="Search"
-                        className="w-[40%] h-8 p-2 mt-2 text-xs bg-zinc-100 border-1 border-zinc-200 outline-0 rounded-md"
+                        className="w-[40%] h-8 p-2 mt-2 text-xs bg-zinc-100 border-1 border-zinc-200 outline-0 rounded-md dark:bg-discord-500 dark:border-transparent dark:text-zinc-100"
                     />
                     <div className={`flex gap-3 pt-2`}>{children}</div>
                 </div>
